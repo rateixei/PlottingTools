8e1854e3955039cb6d0fa5b69b37b90b3a0bd721
Rafael <rateixei@cern.ch>
2015-04-28 11:23:39 +0200

    pull plots

---
+++ PullPlotter.py
@@ -0,0 +1,55 @@
+#!/bin/python
+
+from ROOT import *
+from doPull import *
+import getopt, sys
+
+def main(argv):
+	ROOT.gROOT.SetBatch()
+	st_dataF = ''
+	st_dataN = 'Data'
+	st_mcF = ''
+	st_mcN = 'MC'
+	st_hist = ''
+	st_var = ''
+	is_norm = 1
+	try:
+		opts, args = getopt.getopt(argv,"d:m:n:",["data=","mc=", "histName=", "help", "dataName=", "mcName=", "varName=", "drawNormOff"])
+	except getopt.GetoptError:
+		print 'PullPlotter.py -data <inputfile> -mc <inputfile> -histName <name>'
+		sys.exit(2)
+	if len(opts) == 0:
+		print 'PullPlotter.py -data <inputfile> -mc <inputfile> -histName <name>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'PullPlotter.py -data <inputfile> -mc <inputfile> -histName <name>'
+			sys.exit()
+		elif opt in ("-d", "--data"):
+			st_dataF = arg
+		elif opt in ("-m", "--mc"):
+			st_mcF = arg
+		elif opt in ("-n", "--histName"):
+			st_hist = arg
+		elif opt in ("--dataName"):
+			st_dataN = arg
+		elif opt in ("--mcName"):
+			st_mcN = arg
+		elif opt in ("--varName"):
+			st_var = arg
+		elif opt in ("--drawNormOff"):
+			is_norm = 0
+			
+	if st_dataF == '' or st_mcF == '' or st_hist == '':
+		print 'PullPlotter.py -data <inputfile> -mc <inputfile> -histName <name>'
+		sys.exit(2)
+		
+	dataF = TFile.Open(str(st_dataF), 'READ')
+	dataHist = dataF.Get(str(st_hist))
+	mcF = TFile.Open(str(st_mcF), 'READ')
+	mcHist = mcF.Get(str(st_hist))
+	
+	doPull(dataHist, st_dataN, mcHist, st_mcN, 'minus', st_var, is_norm)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
--- doPull.py
+++ doPull.py
@@ -4,13 +4,14 @@ def doPull(data, dataName, mc, mcName, Type, varName, norm):
 	ROOT.gROOT.SetBatch()
 	data_nbins = data.GetNbinsX()
 	mc_nbins = mc.GetNbinsX()
-
+	
 	data.SetLineWidth(3)
 	data.SetLineColor(1)
 	mc.SetLineWidth(3)
 	mc.SetLineColor(kAzure-3)
 	mc.SetFillColor(kAzure-3)
-
+	mc.SetFillStyle(3251)
+	
 	if norm:
 		tot_data = data.Integral()
 		data.Scale(1./tot_data)
@@ -19,32 +20,39 @@ def doPull(data, dataName, mc, mcName, Type, varName, norm):
 	if( data_nbins != mc_nbins ):
 		print 'Number of bins of histograms being compared are not the same!'
 		return 0
-
+	
 	Type = 'minus'
 	if Type is not 'minus':
 		if Type is not 'over':
 			print 'Type must be either minus or over'
 			return 0
-
+			
 	pull = data.Clone()
 	pull.SetNameTitle("", "")
 	pull.Reset()
-	pull.SetMarkerStyle(4)
-
+	pull.SetMarkerStyle(20)
+	
 	for x in xrange(0,data_nbins):
 		n_data = data.GetBinContent(x+1)
 		n_mc = mc.GetBinContent(x+1)
+#		if n_mc == 0:
+#			continue 
 		diff = n_data - n_mc
+		print diff
 		pull.SetBinContent(x+1, diff)
 		pull.SetBinError(x+1, 0)
-
-
+		
+	
 	max_data = data.GetMaximum()
 	max_mc = mc.GetMaximum()
 	if(max_mc < max_data):
 		mc.GetYaxis().SetRangeUser(0, max_data*1.1)
+		
+	pull_max = pull.GetMaximum()
+	pull_min = pull.GetMinimum()
+#	pull.GetYaxis().SetRangeUser( 100.2, -1.2)	
 
-
+	
 	ratio = 0.25
 	epsilon = 0.10
 	c1 = TCanvas("c1", "c1", 800, 800)
@@ -61,7 +69,7 @@ def doPull(data, dataName, mc, mcName, Type, varName, norm):
 	p2.SetBottomMargin(0.3)
 	p2.SetGridx()
 	p2.SetGridy()
-
+		
 	data.GetYaxis().SetLabelSize(0.025)
 	data.GetXaxis().SetLabelSize(0.0)
 	data.GetYaxis().SetTitleOffset(1.5)
@@ -70,17 +78,17 @@ def doPull(data, dataName, mc, mcName, Type, varName, norm):
 	mc.GetXaxis().SetLabelSize(0.0)
 	mc.GetYaxis().SetTitleOffset(1.5)
 	mc.GetYaxis().SetTitleSize(0.03)
-
+	
 	pull.GetYaxis().SetNdivisions(8)
-
+	
 	if not dataName:
 		pull.GetYaxis().SetTitle("Data - MC")
 	if dataName:
-		st_ypull = str(dataName[0:4]) + '. - ' + str(mcName[0:4]) + '.'
+		st_ypull = str(dataName[0:4]) + ' - ' + str(mcName[0:4]) 
 		pull.GetYaxis().SetTitle(str(st_ypull))
 	pull.GetYaxis().SetTitleSize(0.08)
 	pull.GetYaxis().SetTitleOffset(0.35)
-
+			
 	if not varName:
 		pull.GetXaxis().SetTitle("")
 	if varName:
@@ -90,25 +98,29 @@ def doPull(data, dataName, mc, mcName, Type, varName, norm):
 		data.SetTitle(varName)
 		mc.SetTitle(varName)
 	pull.GetXaxis().SetLabelSize(0.1)
-
+		
 	leg = TLegend(0.6,0.8,0.89,0.89)
 	leg.SetFillStyle(0)
 	leg.SetLineWidth(0)
 	leg.SetBorderSize(0)
+	
+#	pull.GetYaxis().SetLimits(1.2, -1.2)	
+	pull.SetMaximum(0.007)
+	pull.SetMinimum(-0.007)
 
 	if not dataName:
 		leg.AddEntry(data, "Data", "l")
-		leg.AddEntry(mc, "MC", "l")
+		leg.AddEntry(mc, "MC", "f")
 	if dataName:
 		leg.AddEntry(data, str(dataName), "l")
-		leg.AddEntry(mc, str(mcName), "l")
-
+		leg.AddEntry(mc, str(mcName), "f")
+	
 	p1.cd()
 	mc.Draw()
 	data.Draw("same")
 	leg.Draw("same")
 	p2.cd()
-	pull.Draw("p")
+	pull.Draw("p0")
 	c1.cd()
 	p1.Draw()
 	p2.Draw()
